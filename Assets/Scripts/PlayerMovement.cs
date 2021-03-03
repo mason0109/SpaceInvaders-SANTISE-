3498554fc03c01df54a79bf13fdef9ccb4292e86
mason0109 <mason.mason0109@gmail.com>
2021-03-03 17:07:49 +0000

    Player Movement
    Player moves left and right, but without boundaries.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,25 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float runSpeed;
+
     private Rigidbody2D rigidbody;
+    private Vector2 myVelocity;
+    private Vector2 currentInputDirection;
+
+    //to keep the player in bounds?
+    public float maxBound, minBound;
 
     private void Awake()
     {
         rigidbody = gameObject.GetComponent<Rigidbody2D>();
     } 
 
-    // Start is called before the first frame update
-    void Start()
+    public void Move(Vector2 direction)
     {
-        
+        myVelocity = rigidbody.velocity;
+        myVelocity.x = direction.x * runSpeed;
+        rigidbody.velocity = myVelocity;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void onMove(InputAction.CallbackContext input)
     {
-        
+        Move(input.ReadValue<Vector2>());
     }
 }
